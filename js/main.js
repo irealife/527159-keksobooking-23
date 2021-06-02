4462d0bb7c9c9de711a1f40dc8fb53de98d18005
irealife <irealife@rambler.ru>
2021-06-02 19:18:43 +0300

    Функция случайных чисел

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function getRandomInt (min, max) {
+  if (min >= 0 && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  if (min >= max || min < 0) {
+    return alert('Введите правильные значения, где min меньше max и min >= 0');
+  }
+}
+
+getRandomInt();
+
+function getRandomIntFloat (min, max, intFloat) {
+  if (min >= 0 && min < max) {
+    return Number((Math.random() * (max - min + 1) + min).toFixed(intFloat));
+  }
+  if (min >= max || min < 0) {
+    return alert('Введите правильные значения, где min меньше max и min >= 0');
+  }
+}
+
+getRandomIntFloat();
