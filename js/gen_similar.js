99e066d77d1697617d018e19f07c6dba56aa8ec7
irealife <irealife@rambler.ru>
2021-07-07 00:45:56 +0300

    Строковые шаблоны вместо конкатенации

--- js/gen_similar.js
+++ js/gen_similar.js
@@ -32,13 +32,13 @@ const keysOfferType = Object.keys(typeObjectValue);
 const valuesOfferType = Object.values(typeObjectValue);
 
 const getObjectValue = (array, keys, values) => {
-  for (let i = 0; i <= array.length; i++) {
+  for (let arrCounter = 0; arrCounter <= array.length; arrCounter++) {
     let arrType = [];
-    arrType = array[i];
-    for(let j = 0; j <= keys.length; j++) {
-      if (arrType === keys[j]) {
+    arrType = array[arrCounter];
+    for(let arrCounterKey = 0; arrCounterKey <= keys.length; arrCounterKey++) {
+      if (arrType === keys[arrCounterKey]) {
         let typeValue = [];
-        typeValue = values[j];
+        typeValue = values[arrCounterKey];
         return typeValue;
       }
     }
@@ -47,27 +47,27 @@ const getObjectValue = (array, keys, values) => {
 
 //Карточка объявления
 
-for (let i = 0; i <= GET_SIMILAR_AD.length - 1; i++) {
+for (let arrCounterCard = 0; arrCounterCard <= GET_SIMILAR_AD.length - 1; arrCounterCard++) {
   const elementCard = templateCard.cloneNode(true);
   const popupTitle = elementCard.querySelector('.popup__title');
-  popupTitle.textContent = GET_SIMILAR_AD[i].offer.title;
+  popupTitle.textContent = GET_SIMILAR_AD[arrCounterCard].offer.title;
   const popupAddress = elementCard.querySelector('.popup__text--address');
-  popupAddress.textContent = GET_SIMILAR_AD[i].offer.address;
+  popupAddress.textContent = GET_SIMILAR_AD[arrCounterCard].offer.address;
   const popupPrice = elementCard.querySelector('.popup__text--price');
-  popupPrice.textContent = GET_SIMILAR_AD[i].offer.price + ' ₽/ночь';
+  popupPrice.textContent = `${GET_SIMILAR_AD[arrCounterCard].offer.price} ₽/ночь`;
   const popupType = elementCard.querySelector('.popup__type');
   popupType.textContent = getObjectValue(CHOOSE_OFFER_TYPE, keysOfferType, valuesOfferType);
 
   const popupCapacity = elementCard.querySelector('.popup__text--capacity');
-  popupCapacity.textContent = GET_SIMILAR_AD[i].offer.rooms + ' комнаты для ' + GET_SIMILAR_AD[i].offer.guests + ' гостей.';
+  popupCapacity.textContent = `${GET_SIMILAR_AD[arrCounterCard].offer.rooms} комнаты для ${GET_SIMILAR_AD[arrCounterCard].offer.guests} гостей.`;
   const popupTime = elementCard.querySelector('.popup__text--time');
-  popupTime.textContent = 'Заезд после ' + GET_SIMILAR_AD[i].offer.checkin + ', выезд до ' + GET_SIMILAR_AD[i].offer.checkout;
+  popupTime.textContent = `Заезд после ${GET_SIMILAR_AD[arrCounterCard].offer.checkin}, выезд до ${GET_SIMILAR_AD[arrCounterCard].offer.checkout}`;
 
   //Вывод доступных удобств в номере
 
   const popupFeatures = elementCard.querySelector('.popup__features');
 
-  const featuresModifiers = GET_SIMILAR_AD[i].offer.features.map((feature) => `popup__feature--${feature}`);
+  const featuresModifiers = GET_SIMILAR_AD[arrCounterCard].offer.features.map((feature) => `popup__feature--${feature}`);
   popupFeatures.querySelectorAll('.popup__feature').forEach((item) => {
     const featureModifier = item.classList[1];
     if (!featuresModifiers.includes(featureModifier)) {
@@ -77,18 +77,18 @@ for (let i = 0; i <= GET_SIMILAR_AD.length - 1; i++) {
   });
 
   const popupDescription = elementCard.querySelector('.popup__description');
-  popupDescription.textContent = GET_SIMILAR_AD[i].offer.description;
+  popupDescription.textContent = GET_SIMILAR_AD[arrCounterCard].offer.description;
 
   //Вывод всех фото объявления
 
   const popupPhotos = elementCard.querySelector('.popup__photos');
 
   popupPhotos.querySelectorAll('.popup__photo').forEach((item) => {
-    item.src = GET_SIMILAR_AD[i].offer.photos;
+    item.src = GET_SIMILAR_AD[arrCounterCard].offer.photos;
   });
 
   const popupAvatar = elementCard.querySelector('.popup__avatar');
-  popupAvatar.src = GET_SIMILAR_AD[i].author.avatar;
+  popupAvatar.src = GET_SIMILAR_AD[arrCounterCard].author.avatar;
 
 
   cards.push(elementCard);
